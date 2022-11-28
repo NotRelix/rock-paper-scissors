ae1a3dc13ed3af510ad238d64b378fa5543dadc3
Reece Lim <lim.reecesergei@gmail.com>
2022-11-28 11:51:11 +0800

    Loop game 5 times, and Fix bugs

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-Afunction computerSelection()
+function computerSelection()
 {
     let randomNumber = getComputerChoice();
     if (randomNumber === 1)
-        return "rock";
+        return "Rock";
     else if (randomNumber === 2)
-        return "paper";
+        return "Paper";
     else
-        return "scissor";
+        return "Scissor";
 }
 
 function getComputerChoice()
@@ -18,32 +18,39 @@ function getComputerChoice()
 function playerSelection()
 {
     let guess = prompt("Rock, Paper, Scissor? ");
-    return guess.toLowerCase();
+    guess = guess.charAt(0).toUpperCase() + guess.slice(1).toLowerCase();
+    return guess;
 }
 
-function game(player, computer)
+function playRound(player, computer)
 {
-    if (player === "rock" && computer === "scissor")
+    if (player === "Rock" && computer === "Scissor")
         return "You win! Rock beats Scissor";
-    else if (player === "paper" && computer === "rock")
+    else if (player === "Paper" && computer === "Rock")
         return "You win! Paper beats Rock";
-    else if (player === "scissor" && computer === "paper")
+    else if (player === "Scissor" && computer === "Paper")
         return "You win! Scissor beats Paper";
 
     else if (player === computer)
         return "Draw";
     
-    else if (computer === "rock")
+    else if (computer === "Rock")
         return "You lose! Paper beats Rock";
-    else if (computer === "paper")
+    else if (computer === "Paper")
         return "You lose! Scissor beats Paper";
     else
         return "You lose! Rock beats Scissor";
 }
 
-let player = playerSelection();
-let computer = computerSelection();
+function game()
+{
+    for (let i = 0; i < 5; i++)
+    {
+        let player = playerSelection();
+        let computer = computerSelection();
+        console.log(`${player} vs ${computer}`)
+        console.log(playRound(player, computer));
+    }
+}
 
-console.log(`${player} vs ${computer}`)
-
-console.log(game(player, computer));
+game();
